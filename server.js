17488155991910792d1debd6cc4f107ce14443b7
SylviaRemington <sylviaremington@gmail.com>
2025-05-29 21:08:31 -0700

    Exercise 1 completed

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!') // checking to make sure works
 // })
 
-app.get('/greetings/:username', (req, res) => (
-    res.send(`Hello there, ${username}! What a delight to see you once more!`)
-))
+//Exercise 1 - Be Polite, Greet the User
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username; //you have to have req.params.username anytime after using : to define the parameter
+    res.send(`Hello there, ${username}! What a delight to see you once more!`);
+});
+
+//Exercise 2 - Rolling the Dice
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
