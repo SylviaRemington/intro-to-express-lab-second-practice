4cb9f803eeeba16ec5a50d63b5247074e4aa1316
SylviaRemington <sylviaremington@gmail.com>
2025-05-29 20:50:11 -0700

    writing route code

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!') // checking to make sure works
+// })
+
+app.get('/greetings/:username', (req, res) => (
+    res.send(`Hello there, ${username}! What a delight to see you once more!`)
+))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
