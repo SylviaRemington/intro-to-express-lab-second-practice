42b8536e20c76153062b2311c9aab211d6bed3de
SylviaRemington <sylviaremington@gmail.com>
2025-05-30 02:28:50 -0700

    adding addtl code solve versions to exercise 4 section to better understand the code

--- server.js
+++ server.js
@@ -397,6 +397,40 @@ shoeFilter = shoeFilter.filter(function(shoe) {
 });
 */
 
+//Another version:
+// app.get('/shoes', (req, res) => {
+//       const shoes = [
+//       { name: "Birkenstocks", price: 50, type: "sandal" },
+//       { name: "Air Jordans", price: 500, type: "sneaker" },
+//       { name: "Air Mahomeses", price: 501, type: "sneaker" },
+//       { name: "Utility Boots", price: 20, type: "boot" },
+//       { name: "Velcro Sandals", price: 15, type: "sandal" },
+//       { name: "Jet Boots", price: 1000, type: "boot" },
+//       { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+//   ];
+
+//   let filteredShoes = shoes;
+
+//   const minPrice = Number(req.query['min-price']);
+//   const maxPrice = Number(req.query['max-price']);
+//   const type = req.query.type;
+
+//   if (!isNaN(minPrice)) {
+//     filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+//   }
+
+//   if (!isNaN(maxPrice)) {
+//     filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+//   }
+
+//   if (type) {
+//     filteredShoes = filteredShoes.filter(shoe => shoe.type === type);
+//   }
+
+//   res.send(filteredShoes);
+
+// });
+
 //--------------------------------------------------------------------------------------------------------------------------------
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
